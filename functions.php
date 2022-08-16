a3e178e08fd8b6f77c932eda33ea968b49bdae3d
tienhtpd02242 <48255077+thangtienql@users.noreply.github.com>
2022-08-16 15:17:39 +0700

    ccs

--- functions.php
+++ functions.php
@@ -10,4 +10,14 @@
  * @link    https://www.studiopress.com/
  */
 
- require_once get_stylesheet_directory() . '/package-main/init-load.php';
+require_once get_stylesheet_directory() . '/package-main/init-load.php';
+
+function font_thangmayhd( $content, $default_editor ) {
+    if ( 'tinymce' == $default_editor ) {
+        $content = '<div style="font-family: Arial, Helvetica, sans-serif">' . $content . '</div>';
+    }
+    return $content;
+}
+if ( is_admin() ) {
+    add_filter( 'the_editor_content', 'font_thangmayhd', 10, 2 );
+}
